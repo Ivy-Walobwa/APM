24cb39ac6a3ba9f73dcf64d28a375d6ff6a89359
Ivy Walobwa <ivywalobwa@gmail.com>
2019-09-14 22:49:15 +0300

    handled inputs and events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
