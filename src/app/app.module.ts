2a4c2bfae4ad5f0c32a77c88d150f4e8a2784dc8
Ivy Walobwa <ivywalobwa@gmail.com>
2019-09-19 16:49:10 +0300

    navigation and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponents } from './shared/star.component';
-import { from } from 'rxjs';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 
 @NgModule({
@@ -15,12 +17,21 @@ import { from } from 'rxjs';
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponents
+    StarComponents,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path:'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch:'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   bootstrap: [AppComponent]
 })
